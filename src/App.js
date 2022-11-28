bd41d844ad175d7b73037f5ea94098fd4da8e638
Farid0558475484 <faridmh@code.edu.az>
2022-11-28 20:32:58 +0400

    first project test1

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className="firstProject">
+    <div className='header'> 
+    <img src="./logo512.png" alt="logo" />
+    </div>
+    <nav className='nav'>
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Messages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+
+    </nav>
+    <div className='content'>
+
+    </div>
+
+   </div>
+  );
+}
+
+export default App;
