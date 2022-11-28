9360bae037af98016eedcc7000bfca49befc1686
Farid0558475484 <faridmh@code.edu.az>
2022-11-28 22:55:07 +0400

    css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-   <div className="firstProject">
-       <Header />
-       <Navbar />
-       <Profile />
-   </div>
+    <div className="firstProject">
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
   );
 }
 
