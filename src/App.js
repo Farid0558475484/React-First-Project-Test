fc71059b75727f4bcf8c5f75177c2b7ae0010b8f
Farid0558475484 <faridmh@code.edu.az>
2022-11-30 02:40:50 +0400

    dialogs components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/index.jsx";
 
 function App() {
   return (
     <div className="firstProject">
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <Dialogs />
     </div>
   );
 }
---
+++ src/components/Dialogs/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./style.module.scss";
+
+function Dialogs() {
+  return;
+  <div className="content">
+    
+  </div>;
+}
+
+export default Dialogs;
