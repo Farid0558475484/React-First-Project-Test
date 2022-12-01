9ac21d5c05f2e5bda18a92b7ffdda7a028b147a2
Farid0558475484 <faridmh@code.edu.az>
2022-12-01 14:46:34 +0400

    map metod array

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -25,27 +25,59 @@ function Dialogs() {
     { name: "Vagif", id: 6 },
     { name: "Nicat", id: 7 },
   ];
+  // [
+  //   <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />,
+  //   <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />,
+  //   <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />,
+  //   <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />,
+  //   <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />,
+  //   <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />,
+  //   <DialogItem name={dialogsData[6].name} id={dialogsData[6].id} />,
+  //   <DialogItem name={dialogsData[7].name} id={dialogsData[7].id} />,
+  // ];
   let messagesData = [
     { message: "hi", id: 1 },
     { message: "How are you?", id: 2 },
     { message: "Fine", id: 3 },
   ];
+
+  let dialogsElements = dialogsData.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+
+  let messagesElements = messagesData.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
+
+  // [
+  //   <Message message={messagesData[0].message} id={messagesData[0].id} />,
+  //   <Message message={messagesData[1].message} id={messagesData[1].id} />,
+  //   <Message message={messagesData[2].message} id={messagesData[2].id} />,
+  // ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+
+
+        {dialogsElements}
+
+        {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
         <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
         <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
         <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
         <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
         <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-        <DialogItem name={dialogsData[6].name} id={dialogsData[6].id} />
-
+        <DialogItem name={dialogsData[6].name} id={dialogsData[6].id} /> */}
       </div>
       <div className={s.messages}>
-        <Message message={messagesData[0].message} id={messagesData[0].id} />
+
+        {messagesElements}
+
+        {/* <Message message={messagesData[0].message} id={messagesData[0].id} />
         <Message message={messagesData[1].message} id={messagesData[1].id} />
-        <Message message={messagesData[2].message} id={messagesData[2].id} />
+        <Message message={messagesData[2].message} id={messagesData[2].id} /> */}
       </div>
     </div>
   );
--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -3,11 +3,16 @@ import s from "./MyPost.module.scss";
 import Post from "./Post/Post";
 
 function MyPost() {
-  let postData = [
+  let post = [
     { message: "Hi,My name Farid///its props", id: 1 },
     { message: "my favorite hobbi is writing code//its props", id: 2 },
+    { message: "Marketing, SEO, Veb-sayt", id: 3 },
+    { message: "Flegri.az", id: 4 },
+
   ];
-  
+
+  let postElements = post.map((p) => <Post message={p.message} />);
+
   return (
     <div className={s.mypost}>
       <textarea></textarea>
@@ -16,9 +21,10 @@ function MyPost() {
         <button>Sign in</button>
       </div>
       <p className={s.item}>Sagol</p>
-
-      <Post message={postData[0].message} />
-      <Post message={postData[1].message} />
+      
+      {postElements}
+      {/* <Post message={postData[0].message} />
+      <Post message={postData[1].message} /> */}
     </div>
   );
 }
