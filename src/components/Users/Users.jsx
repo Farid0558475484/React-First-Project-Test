2e52f93ea7159e067c010a38e2ea78f40b5a4984
Farid0558475484 <faridmh@code.edu.az>
2023-01-08 14:50:05 +0400

    side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,19 @@ import * as axios from "axios";
 import userPhoto from "./../../img/user.png";
 
 function Users(props) {
+    const getUsers = () => {
   
     axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
         props.setUsers(response.data.items);
     });
-
+    }
 
 
   return (
+
+
     <div>
+           < button onClick={getUsers}>Get Users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span><div><img className={styles.photos} src={u.photos !=null ? u.photos.small : userPhoto } alt="logo" /></div>
