ce4c029ce54445a20b52ff708956b48596b839d7
Farid0558475484 <faridmh@code.edu.az>
2023-03-24 16:06:20 +0400

    feat:useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+
 
 const ProfileStatusWithHooks = (props) => {
 
   const [editMode, setEditMode] = useState(false);
   const [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
+
 
   const activateEditMode = () => {
     setEditMode(true);
