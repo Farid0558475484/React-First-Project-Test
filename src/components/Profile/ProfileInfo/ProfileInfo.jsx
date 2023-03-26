11b214cad86657375e7ea76ab689d87877f79068
Farid0558475484 <faridmh@code.edu.az>
2023-03-26 18:44:18 +0400

    feat:start ProfileData

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -29,6 +29,17 @@ const ProfileInfo = (props) => {
           </div>
         )}
 
+        <div className={s.descriptionBlock}>
+          <div className={s.fullName}>{props.profile.fullName}</div>
+          <div className={s.aboutMe}>{props.profile.aboutMe}</div>
+          <div className={s.lookingForAJob}>
+            {props.profile.lookingForAJob ? "Looking for a job" : ""}
+          </div>
+          <div className={s.lookingForAJobDescription}>
+            {props.profile.lookingForAJobDescription}
+          </div>
+        </div>
+
         <ProfileStatusWithHooks
           status={props.status}
           updateStatus={props.updateStatus}
@@ -37,5 +48,47 @@ const ProfileInfo = (props) => {
     </>
   );
 };
+
+// const ProfileData = (props) => {
+//   return (
+//     <div>
+//       <div>
+//         <b>Full name</b>: {props.profile.fullName}
+//       </div>
+//       <div>
+//         <b>Looking for a job</b>: {props.profile.lookingForAJob ? "yes" : "no"}
+//       </div>
+//       {props.profile.lookingForAJob && (
+//         <div>
+//           <b>My professional skills</b>:{" "}
+//           {props.profile.lookingForAJobDescription}
+//         </div>
+//       )}
+//       <div>
+//         <b>About me</b>: {props.profile.aboutMe}
+//       </div>
+//       <div>
+//         <b>Contacts</b>:{" "}
+//         {Object.keys(props.profile.contacts).map((key) => {
+//           return (
+//             <Contact
+//               key={key}
+//               contactTitle={key}
+//               contactValue={props.profile.contacts[key]}
+//             />
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// const Contact = ({ contactTitle, contactValue }) => {
+//   return (
+//     <div className={s.contact}>
+//       <b>{contactTitle}</b>: {contactValue}
+//     </div>
+//   );
+// };
 
 export default ProfileInfo;
