70900b13610ce9a6a769f176cf09f480f250501d
Farid0558475484 <faridmh@code.edu.az>
2023-04-30 22:27:39 +0400

    feat:update user-selector to Ts

---
+++ src/redux/users-selectors.ts
@@ -0,0 +1,34 @@
+import { AppStateType } from "./redux-store";
+
+const getUsersSelector = (state: AppStateType) => {
+  return state.usersPage.users;
+};
+
+const getUsers = (state: AppStateType) => {
+  return state.usersPage.users.filter((u) => true);
+};
+const getPageSize = (state: AppStateType) => {
+  return state.usersPage.pageSize;
+};
+const getTotalUsersCount = (state: AppStateType) => {
+  return state.usersPage.totalUsersCount;
+};
+const getCurrentPage = (state: AppStateType) => {
+  return state.usersPage.currentPage;
+};
+const getIsFetching = (state: AppStateType) => {
+  return state.usersPage.isFetching;
+};
+const getFollowingInProgress = (state: AppStateType) => {
+  return state.usersPage.followingInProgress;
+};
+
+export {
+  getUsers,
+  getPageSize,
+  getTotalUsersCount,
+  getCurrentPage,
+  getIsFetching,
+  getFollowingInProgress,
+  getUsersSelector,
+};
